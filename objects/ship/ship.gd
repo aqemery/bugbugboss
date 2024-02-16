96e52791b0198c9b7bc09fd75453f5ed0619caf0
Adam Emery <aqemery@gmail.com>
2024-02-16 09:56:24 -0500

    clean up

--- objects/ship/ship.gd
+++ objects/ship/ship.gd
@@ -2,36 +2,26 @@ extends Area2D
 
 const SPEED = 50
 const COOLDOWN = 0.25
-
 var _cool:float = 0
 var _can_fire: bool = true
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func _physics_process(delta):
-	var move = Input.get_axis("ui_left", "ui_right")
-	position.x += move * SPEED * delta
-	_cool -= delta
-	
-	keep_onscreen()
-	if Input.is_action_pressed("ui_up"):
-		fire()
+    var move = Input.get_axis("ui_left", "ui_right")
+    position.x += move * SPEED * delta
+    _cool -= delta
+    keep_onscreen()
+    if Input.is_action_pressed("ui_up"):
+        fire()
 
 func keep_onscreen():
-	var end = get_viewport().size
-	position.x = clamp(position.x, 4, 120)
+    var end = get_viewport().size
+    position.x = clamp(position.x, 4, 120)
 
 func fire():
-	if _cool <= 0:
-		ObjectMaker.create_bullet(global_position + Vector2(0, -4))
-		_cool = COOLDOWN
-		
-
-
+    if _cool <= 0:
+        ObjectMaker.create_bullet(global_position + Vector2(0, -4))
+        _cool = COOLDOWN
 
 func _on_area_entered(area):
-	queue_free()
-	pass # Replace with function body.
+    queue_free()
+    pass # Replace with function body.
--- objects/splat/splat.gd
+++ objects/splat/splat.gd
@@ -1,17 +1,9 @@
 extends AnimatedSprite2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():	
-	var name = "splat_%d" % randi_range(1,2)
-	play(name)
-	
-	#await("animation_finished")
-	#queue_free()
-
-
+    var name = "splat_%d" % randi_range(1,2)
+    play(name)
 
 func _on_animation_finished():
-	#queue_free()
-	call_deferred("queue_free")
-
+    #queue_free()
+    call_deferred("queue_free")
