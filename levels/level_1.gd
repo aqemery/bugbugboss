68c663e123f5fb71e78d6b3cd3f80114a2c0c41f
Adam Emery <aqemery@gmail.com>
2024-02-24 17:31:36 -0500

    boss script

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -7,19 +7,15 @@ func _ready():
     
     # create a bunch of bugs evenly spaced around the screen
     var bug = load("res://objects/bug/bug.tscn")
-    for i in range(0, 10):
-        var bs = bug.instantiate()
-        bs.position = Vector2(6 + i * 10, 10)
-        add_child(bs)
+    #for i in range(0, 10):
+        #var bs = bug.instantiate()
+        #bs.position = Vector2(6 + i * 10, 10)
+        #add_child(bs)
 
     # make them all fly around randomly
     for c in get_children():
         var t:Animator = animator.create(c)
         t.fly(Vector2(randi() % 128, 4), 2)
 
-    # set up the camera
-    var camera = get_node("Camera2D")
-    camera.position = Vector2(64, 64)
-    camera.zoom = 1.0
     
 
---
+++ objects/boss/boss.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var health = 20
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+    pass
+
+
+func _on_area_entered(_area):
+    health -= 1
+    if health <= 0:
+        ObjectMaker.create_splat(global_position + Vector2(4,-4))
+        ObjectMaker.create_splat(global_position + Vector2(-4,-4))
+        ObjectMaker.create_splat(global_position + Vector2(4,4))
+        ObjectMaker.create_splat(global_position + Vector2(-4,4))
+        ObjectMaker.create_explosion(position, 0.1, Color.GREEN)
+        queue_free()
+    pass # Replace with function body.
